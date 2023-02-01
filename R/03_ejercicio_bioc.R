a9804cb753a3b95e6ba1b1064eab098c3ca62f88
Hector Ulises Gaspar <hectorgasp@gmail.com>
2023-02-01 09:06:51 -0600

    Se sube el ejercicio de ayer

--- R/03_ejercicio_bioc.R
+++ R/03_ejercicio_bioc.R
@@ -1,1 +1,14 @@
-## TODO
+#Bioconductor 3.16
+
+#crisprDesign
+
+#Autores: Hoberecht L, Perampalam P, Lun A, Fortin J
+
+#Este software nos permite diseñar todo tipo de RNAs guias de CRISPR.
+#Tambien está disponible para 5 tipos de modificaciones que puede hacer CRISPR
+#Y también está disponible para todos los tipos de nucleasas.
+
+#Este diseño de RNAs guias se puede hacer en todo tipo de infromacion genomica:
+#genomas de referencia, transcriptomas, y secuencias personalizadas
+
+#Este software aun es reciente, esta en su primera version y lleva 6 meses, por lo que aun no tiene pregunas
