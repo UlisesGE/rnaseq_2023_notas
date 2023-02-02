aaa1d671de8790bacfeeed84824fce5359369474
Hector Ulises Gaspar <hectorgasp@gmail.com>
2023-02-02 10:34:46 -0600

    Se agrega el ejercicio del tema 5

---
+++ R/ejercicio_5.1.R
@@ -0,0 +1,6 @@
+# En la segunda columna de la tabla se encuentra el campo Response.  Resistant, pre treatment
+
+
+#En el ejercicio 3, si no se tuviera el 0 en la formula, se consideraraía el intercepto en el modelo
+#Esto no es recomendado cuando en el modelo se usan batches, debido a que se perdería el primer batch.
+#Con la perdida de este batch, se modifica toda la interpretación.
